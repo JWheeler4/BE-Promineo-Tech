94aa052162a28a907b3b2fea96f9ad7a27b8791f
JWheeler4 <60630184+JWheeler4@users.noreply.github.com>
2022-11-21 13:33:26 -0700

    Added week 10 content

--- mysql-java-video/src/main/java/recipes/dao/Recipes.java
+++ mysql-java-video/src/main/java/recipes/dao/Recipes.java
@@ -14,11 +14,14 @@ import recipes.service.RecipeService;
 public class Recipes {
 	private Scanner scanner = new Scanner(System.in);
 	private RecipeService recipeService = new RecipeService();
+	private Recipe curRecipe;
 
 //	@formatter:off
 	private List<String> operations = List.of(
 			"1) Create and populate all tables",
-			"2) Add a recipe"
+			"2) Add a recipe",
+			"3) List reicpes",
+			"4) Select working recipe"
 	);
 //	@formatter:on
 
@@ -40,9 +43,17 @@ public class Recipes {
 				case 1:
 					createTables();
 					break;
-					
+
 				case 2:
 					addRecipe();
+					break;
+
+				case 3:
+					listRecipes();
+					break;
+
+				case 4:
+					setCurrentRecipe();
 					break;
 
 				default:
@@ -55,33 +66,64 @@ public class Recipes {
 		}
 	}
 
+	private void setCurrentRecipe() {
+		List<Recipe> recipes = listRecipes();
+
+		Integer recipeId = getIntInput("Select a recipe ID");
+
+		curRecipe = null;
+
+		for (Recipe recipe : recipes) {
+			if (recipe.getRecipeId().equals(recipeId)) {
+				curRecipe = recipeService.fetchRecipeById(recipeId);
+				break;
+			}
+		}
+
+		if (Objects.isNull(curRecipe)) {
+			System.out.println("\nInvalid recipe selected.");
+		}
+	}
+
+	private List<Recipe> listRecipes() {
+		List<Recipe> recipes = recipeService.fetchRecipes();
+
+		System.out.println("\nRecipes: ");
+
+		recipes.forEach(recipe -> System.out.println("   " + recipe.getRecipeId() + ": " + recipe.getRecipeName()));
+
+		return recipes;
+	}
+
 	private void addRecipe() {
 		String name = getStringInput("Enter the recipe name");
 		String notes = getStringInput("Enter the recipe notes");
 		Integer numServings = getIntInput("Enter number of servings");
 		Integer prepMinutes = getIntInput("Enter prep time in minutes");
 		Integer cookMinutes = getIntInput("Enter cook time in minutes");
-		
+
 		LocalTime prepTime = minutesToLocalTime(prepMinutes);
 		LocalTime cookTime = minutesToLocalTime(cookMinutes);
-		
+
 		Recipe recipe = new Recipe();
-		
+
 		recipe.setRecipeName(name);
 		recipe.setNotes(notes);
 		recipe.setNumServings(numServings);
 		recipe.setPrepTime(prepTime);
 		recipe.setCookTime(cookTime);
-		
+
 		Recipe dbRecipe = recipeService.addRecipe(recipe);
 		System.out.println("You added this recipe:\n" + dbRecipe);
+
+		curRecipe = recipeService.fetchRecipeById(dbRecipe.getRecipeId());
 	}
 
 	private LocalTime minutesToLocalTime(Integer numMinutes) {
 		int min = Objects.isNull(numMinutes) ? 0 : numMinutes;
 		int hours = min / 60;
 		int minutes = min % 60;
-		
+
 		return LocalTime.of(hours, minutes);
 	}
 
@@ -103,6 +145,13 @@ public class Recipes {
 	}
 
 	private void printOperations() {
+		System.out.println();
+		if (Objects.isNull(curRecipe)) {
+			System.out.println("You are not working with a recipe.");
+		} else {
+			System.out.println("You are working with " + curRecipe);
+		}
+
 		System.out.println();
 		System.out.println("Here's what you can do:");
 
--- mysql-java-video/src/main/java/recipes/service/RecipeService.java
+++ mysql-java-video/src/main/java/recipes/service/RecipeService.java
@@ -7,6 +7,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import recipes.dao.RecipeDao;
 import recipes.entity.Recipe;
 import recipes.exception.DbException;
@@ -14,9 +16,14 @@ import recipes.exception.DbException;
 public class RecipeService {
 	private static final String SCHEMA_FILE = "recipe_schema.sql";
 	private static final String DATA_FILE = "recipe_data.sql";
-	
+
 	private RecipeDao recipeDao = new RecipeDao();
-	
+
+	public Recipe fetchRecipeById(Integer recipeId) {
+		return recipeDao.fetchRecipeById(recipeId)
+				.orElseThrow(() -> new NoSuchElementException("Recipe with ID=" + recipeId + " does not exist."));
+	}
+
 	public void createAndPopulateTables() {
 		loadFromFile(SCHEMA_FILE);
 		loadFromFile(DATA_FILE);
@@ -25,37 +32,37 @@ public class RecipeService {
 	private void loadFromFile(String fileName) {
 		String content = readFileContent(fileName);
 		List<String> sqlStatements = convertContentToSqlStatements(content);
-		
+
 		sqlStatements.forEach(line -> System.out.println(line));
-		
+
 		recipeDao.executeBatch(sqlStatements);
 	}
 
 	private List<String> convertContentToSqlStatements(String content) {
 		content = removeComments(content);
 		content = replaceWhitespaceSequencesWithSingleSpace(content);
-		
+
 		return extractLinesFromContent(content);
 	}
 
 	private List<String> extractLinesFromContent(String content) {
 		List<String> lines = new LinkedList<>();
-		
+
 		while (!content.isEmpty()) {
 			int semicolon = content.indexOf(";");
-			
+
 			if (semicolon == -1) {
 				if (!content.isBlank()) {
 					lines.add(content);
 				}
-				
+
 				content = "";
 			} else {
 				lines.add(content.substring(0, semicolon).trim());
 				content = content.substring(semicolon + 1);
 			}
 		}
-		
+
 		return lines;
 	}
 
@@ -66,17 +73,17 @@ public class RecipeService {
 	private String removeComments(String content) {
 		StringBuilder builder = new StringBuilder(content);
 		int commentPos = 0;
-		
-		while((commentPos = builder.indexOf("-- ", commentPos)) != -1) {
+
+		while ((commentPos = builder.indexOf("-- ", commentPos)) != -1) {
 			int eolPos = builder.indexOf("\n", commentPos + 1);
-			
+
 			if (eolPos == -1) {
 				builder.replace(commentPos, builder.length(), "");
 			} else {
 				builder.replace(commentPos, eolPos + 1, "");
 			}
 		}
-		
+
 		return builder.toString();
 	}
 
@@ -88,12 +95,16 @@ public class RecipeService {
 			throw new DbException(e);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new RecipeService().createAndPopulateTables();
 	}
 
 	public Recipe addRecipe(Recipe recipe) {
 		return recipeDao.insertRecipe(recipe);
+	}
+
+	public List<Recipe> fetchRecipes() {
+		return recipeDao.fetchAllRecipes();
 	}
 }
